91e12146c1fcc03a17539b26ba1ef7e70dfc5c4c
Saikou Gassama <gassamasaikou@yahoo.com>
2023-11-20 21:28:15 +0000

    added client origin dev

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -122,6 +122,10 @@ MIDDLEWARE = [
 
 # Assuming CLIENT_ORIGIN is something like "https://subdomain.gitpod.io"
 if 'CLIENT_ORIGIN' in os.environ:
+    CORS_ALLOWED_ORIGINS = [
+        os.environ.get('CLIENT_ORIGIN')
+    ]
+elif 'CLIENT_ORIGIN_DEV' in os.environ:
     extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
         rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
