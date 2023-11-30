1711dce1e21fbcc92b71e36550e8ab6bc236e763
Saikou Gassama <gassamasaikou@yahoo.com>
2023-11-30 21:29:10 +0000

    complied frontend to backend

--- frontend/src/api/axiosDefaults.js
+++ frontend/src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://drfapi90-4efd6b4b76d8.herokuapp.com/';
+// axios.defaults.baseURL = 'https://drfapi90-4efd6b4b76d8.herokuapp.com/';
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
 
